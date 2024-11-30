ed3705c8bfc607a478187f585346e1a64dd6184d
Ajama0 <abasjama04@hotmail.com>
2024-11-30 13:14:42 +0000

    add custom exception for entity

---
+++ src/main/java/com/example/Elite/Edge/Properties/Exceptions/tenantNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.Elite.Edge.Properties.Exceptions;
+
+public class tenantNotFoundException extends RuntimeException{
+
+    public tenantNotFoundException(String message){
+        super(message);
+    }
+}
